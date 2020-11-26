d6bc5f89bfe0743957eb153d1088acedc00cceb7
I29273蔡賜益 <leo8126@icsc.com.tw>
2020-11-26 14:27:53 +0800

    create deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# region check alive
+until [ `curl  -o /dev/null -I -L -s --connect-timeout 1 -w %{http_code} http://192.168.56.108` == "200" ];
+do
+  echo "service prepare to start..."
+  sleep 5
+done
+# endregion
+
+# region
+# scp ./target/my-app.war root@192.168.56.108:/opt/apache-tomcat-8.5.60/webapps
+echo 'deploy complete!'
+# endregion
