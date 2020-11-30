70b8767e59b090359d30f5f44f7de7728be56c0f
I29273蔡賜益 <leo8126@icsc.com.tw>
2020-11-30 21:09:00 +0800

    update deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-# region deploy
+# region transfer artifact and then restart server
 scp ./target/my-app.war root@192.168.56.108:/opt/apache-tomcat-8.5.60/webapps
+service tomcat start
 # endregion
 
-# region check alive
+# region wait the sevrice, check it alive
 until [ `curl  -o /dev/null -I -L -s --connect-timeout 1 -w %{http_code} http://192.168.56.108` == "200" ];
 do
   echo "service prepare to start..."
   sleep 5
 done
+echo 'service is up!'
 # endregion
--- script/pre-deploy.sh
+++ script/pre-deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-# region shutdown tomcat, remove old app, restart tomcat
+# region shutdown tomcat, remove old app
 service tomcat stop
 ps -ef | grep tomcat | awk '{print $2}' | xargs kill -9
 rm -rf /opt/apache-tomcat-8.5.60/webapps/my-app.*
-service tomcat start
 # endregion
 
