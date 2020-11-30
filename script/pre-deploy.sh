54935cbb8a95a682fcd8cbb34a7bc271f22a6284
I29273蔡賜益 <leo8126@icsc.com.tw>
2020-11-30 23:24:06 +0800

    update deploy script

--- script/pre-deploy.sh
+++ script/pre-deploy.sh
@@ -2,6 +2,13 @@
 
 # region shutdown tomcat, remove old app
 service tomcat stop
+
+until [ -z `ps -ef | grep tomcat | grep -v grep | awk '{print $2}'` ];
+do
+  echo "wait for service to die..."
+  sleep 10
+done
+echo "service down"
 # ps -ef | grep tomcat | grep -v grep | awk '{print $2}' | xargs kill -9
 rm -rf /opt/apache-tomcat-8.5.60/webapps/my-app.*
 # endregion
