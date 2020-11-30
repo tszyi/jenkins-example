a17842c78477c2a252a5b8e67f537e387bbbe6ad
I29273蔡賜益 <leo8126@icsc.com.tw>
2020-11-30 21:55:55 +0800

    update jen

--- script/pre-deploy.sh
+++ script/pre-deploy.sh
@@ -2,7 +2,7 @@
 
 # region shutdown tomcat, remove old app
 service tomcat stop
-ps -ef | grep tomcat | awk '{print $2}' | xargs kill -9
+# ps -ef | grep tomcat | awk '{print $2}' | xargs kill -9
 rm -rf /opt/apache-tomcat-8.5.60/webapps/my-app.*
 # endregion
 
